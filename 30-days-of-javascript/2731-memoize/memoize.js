b2eefa27c8de25785eab35d88ce551526af3f3fb
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-01-15 18:54:43 +0600

    Time: 269 ms (47.08%) | Memory: 96.7 MB (25.78%) - LeetSync

---
+++ 30-days-of-javascript/2731-memoize/memoize.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+    return function(...args) {
+        const key = JSON.stringify(args);
+        // console.log(key);
+        if(cache.has(key)) {
+            return cache.get(key);
+        } else {
+            let ret = fn(...args);
+            cache.set(key, ret);
+            return ret;
+        }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
