3c7625210a9b0d68729c5224c9f6b6921829cf21
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-01-28 22:16:28 +0600

    Time: 50 ms (21.94%) | Memory: 56 MB (7.20%) - LeetSync

---
+++ 30-days-of-javascript-TS-solutions/2746-filter-elements-from-array/filter-elements-from-array.ts
@@ -0,0 +1,12 @@
+type Fn = (n: number, i: number) => any
+
+function filter(arr: number[], fn: Fn): number[] {
+    const res: number[] = [];
+    for(let i: number = 0; i < arr.length; i++) {
+        if(fn(arr[i], i)) {
+            res.push(arr[i]);
+        }
+        
+    }
+    return res;
+};
